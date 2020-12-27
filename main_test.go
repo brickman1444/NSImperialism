d7f8e700fe0123a96bf89edd8b04c347c8ed6b63
Zac Gross <brickman1444@gmail.com>
2020-12-27 13:24:55 -0600

    Start html template with example from https://freshman.tech/web-development-with-go/
    Add test to verify the html templates parse successfully. Looks like it won't be able to catch all html formatting errors but it's better than nothing.

--- main_test.go
+++ main_test.go
@@ -1,12 +1,31 @@
 package main_test
 
 import (
+	"html/template"
+	"io/ioutil"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMyFunc(t *testing.T) {
+func TestParseHTMLTemplates(t *testing.T) {
 
-	assert.Equal(t, 1, 1)
+	allFiles, err := ioutil.ReadDir(".")
+	assert.NoError(t, err)
+
+	htmlFileNames := make([]string, 0)
+	for _, fileInfo := range allFiles {
+		if strings.HasSuffix(fileInfo.Name(), ".html") {
+			htmlFileNames = append(htmlFileNames, fileInfo.Name())
+		}
+	}
+	assert.NotEmpty(t, htmlFileNames)
+
+	for _, htmlFileName := range htmlFileNames {
+		t.Run(htmlFileName, func(t *testing.T) {
+			_, err := template.ParseFiles(htmlFileName)
+			assert.NoError(t, err)
+		})
+	}
 }
