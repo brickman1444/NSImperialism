6ab7656a8ed68729936052a04082eceeb33b53b4
Zac Gross <brickman1444@gmail.com>
2021-01-18 21:01:50 -0600

    Implement war database provider but don't hook it in yet

--- dynamodbwrapper/dynamodbwrapper.go
+++ dynamodbwrapper/dynamodbwrapper.go
@@ -48,6 +48,7 @@ func PutCell(cell DatabaseCell) error {
 		return err
 	}
 
+	log.Println("DynamoDB: Put on cell table")
 	_, err = dynamodbClient.PutItem(databaseContext, &dynamodb.PutItemInput{
 		TableName: aws.String(cellTableName()),
 		Item:      itemToPutMap,
@@ -56,6 +57,8 @@ func PutCell(cell DatabaseCell) error {
 }
 
 func GetCell(territoryName string) (DatabaseCell, error) {
+
+	log.Println("DynamoDB: Get on cell table")
 	getItemOutput, err := dynamodbClient.GetItem(databaseContext, &dynamodb.GetItemInput{
 		TableName: aws.String(cellTableName()),
 		Key: map[string]types.AttributeValue{
@@ -79,4 +82,63 @@ func GetCell(territoryName string) (DatabaseCell, error) {
 	}
 
 	return gotItem, nil
+}
+
+type DatabaseWar struct {
+	Attacker      string
+	Defender      string
+	Score         int
+	ID            string
+	TerritoryName string
+	IsOngoing     bool
+}
+
+func warTableName() string {
+	environmentVariableValue, doesEnvironmentVariableExist := os.LookupEnv("WAR_TABLE_NAME")
+	if doesEnvironmentVariableExist {
+		return environmentVariableValue
+	}
+
+	return "nsimperialism-war"
+}
+
+func PutWars(wars []DatabaseWar) error {
+
+	for _, warToAdd := range wars {
+		itemToPutMap, err := attributevalue.MarshalMap(warToAdd)
+		if err != nil {
+			return err
+		}
+
+		log.Println("DynamoDB: Put on war table")
+		_, err = dynamodbClient.PutItem(databaseContext, &dynamodb.PutItemInput{
+			TableName: aws.String(warTableName()),
+			Item:      itemToPutMap,
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func GetWars() ([]DatabaseWar, error) {
+
+	log.Println("DynamoDB: Scan on war table")
+	scanOutput, err := dynamodbClient.Scan(databaseContext, &dynamodb.ScanInput{
+		TableName:      aws.String(warTableName()),
+		ConsistentRead: aws.Bool(true),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	warsToReturn := []DatabaseWar{}
+	err = attributevalue.UnmarshalListOfMaps(scanOutput.Items, &warsToReturn)
+	if err != nil {
+		return nil, err
+	}
+
+	return warsToReturn, nil
 }
--- war/provider.go
+++ war/provider.go
@@ -1,5 +1,12 @@
 package war
 
+import (
+	"errors"
+
+	"github.com/brickman1444/NSImperialism/dynamodbwrapper"
+	"github.com/brickman1444/NSImperialism/nationstates_api"
+)
+
 type WarProviderInterface interface {
 	GetWars() ([]War, error)
 	PutWars(wars []War) error
@@ -37,3 +44,67 @@ func (simpleList *WarProviderSimpleList) PutWars(warsToAdd []War) error {
 }
 
 var warProviderSimpleListInterfaceChecker WarProviderInterface = &WarProviderSimpleList{}
+
+type WarProviderDatabase struct {
+}
+
+func (provider WarProviderDatabase) GetWars() ([]War, error) {
+
+	databaseWars, err := dynamodbwrapper.GetWars()
+	if err != nil {
+		return nil, err
+	}
+
+	warsToReturn := make([]War, 0, len(databaseWars))
+	for _, databaseWar := range databaseWars {
+
+		attacker, err := nationstates_api.GetNationData(databaseWar.Attacker)
+		if err != nil {
+			return nil, err
+		}
+
+		if attacker == nil {
+			return nil, errors.New("Null attacker")
+		}
+
+		defender, err := nationstates_api.GetNationData(databaseWar.Defender)
+		if err != nil {
+			return nil, err
+		}
+
+		if defender == nil {
+			return nil, errors.New("Null defender")
+		}
+
+		retrievedWar := NewWar(attacker, defender, databaseWar.ID, databaseWar.TerritoryName)
+		retrievedWar.IsOngoing = databaseWar.IsOngoing
+		retrievedWar.Score = databaseWar.Score
+
+		warsToReturn = append(warsToReturn, retrievedWar)
+	}
+
+	return warsToReturn, nil
+}
+
+func DatabaseWarFromRuntimeWar(war War) dynamodbwrapper.DatabaseWar {
+	return dynamodbwrapper.DatabaseWar{
+		Attacker:      war.Attacker.Id,
+		Defender:      war.Defender.Id,
+		Score:         war.Score,
+		ID:            war.Name,
+		TerritoryName: war.TerritoryName,
+		IsOngoing:     war.IsOngoing,
+	}
+}
+
+func (provider WarProviderDatabase) PutWars(warsToAdd []War) error {
+
+	databaseWarsToPut := make([]dynamodbwrapper.DatabaseWar, 0, len(warsToAdd))
+	for _, warToAdd := range warsToAdd {
+		databaseWarsToPut = append(databaseWarsToPut, DatabaseWarFromRuntimeWar(warToAdd))
+	}
+
+	return dynamodbwrapper.PutWars(databaseWarsToPut)
+}
+
+var warProviderDatabaseInterfaceChecker WarProviderInterface = WarProviderDatabase{}
